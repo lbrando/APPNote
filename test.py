d1b914c84c2f8968aacb56508799430018fa15bd
Bruno Maria Di Maio <bruno.maria.dm@gmail.com>
2024-10-14 23:35:17 +0200

    Aggiornamento a classe
    Conversione del codice per l'interfaccia grafica in classe e ottimizzazione.

--- test.py
+++ test.py
@@ -1,47 +1,44 @@
-import tkinter as ctk
+import tkinter as tk
 
-# Funzione chiamata dal pulsante
-def funzione_chiamata():
-    print("Chiamata")
+class App(tk.Tk):
+    def __init__(self, window_width, window_height):
+        super().__init__()
+        
+        # Titolo della finestra
+        self.title("Titolo")
+        # Dimensione della finestra
+        self.geometry(f"{window_width}x{window_height}")
 
-# Definizione della finestra
-window = ctk.Tk()
+        # Do un peso uguale alle colonne definite nella tupla in modo che queste siano identiche come grandezza
+        self.grid_columnconfigure((0, 1), weight=1)
 
-# Titolo della finestra
-window.title("Titolo")
+        # Definizione del bottone
+        # Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
+        # Dando l'attributo 'sticky="ew"' (che significa east e west) faccio in modo che prenda tutto lo spazio orizzontale disponibile
+        self.button = tk.Button(self, text="Testo Bottone", command=self.funzione_chiamata).grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 
-# Dimensione della finestra
-window.geometry("500x200")
+        # Definizione del bottone
+        # Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
+        self.button2 = tk.Button(self, text="Testo", command=self.funzione_chiamata).grid(row=0, column=1, padx=20, pady=20)
 
-# Do un peso uguale alle colonne definite nella tupla in modo che queste siano identiche come grandezza
-window.grid_columnconfigure((0, 1), weight=1)
+        # Definizione del bottone
+        # Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
+        # Dando l'attributo 'sticky="ew"' (che significa east e west) faccio in modo che prenda tutto lo spazio orizzontale disponibile
+        # Con columnspan=2 definisco che questa riga si "espande" su 2 colonne, posso fare lo stesso con le righe tramite 'rawspan'
+        self.button3 = tk.Button(self, text="Testone", command=self.funzione_chiamata).grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 
-# Definizione del bottone
-button = ctk.Button(window, text="Testo Bottone", command=funzione_chiamata)
-# Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
-# Dando l'attributo 'sticky="ew"' (che significa east e west) faccio in modo che prenda tutto lo spazio orizzontale disponibile
-button.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+        # Definizione della checkbox
+        # Inserisco la checkbox nella griflia, facendola "attaccare" sul lato a est, il padding y ha come valore una tupla, in modo da averlo solo sotto
+        self.checkbox_1 = tk.Checkbutton(self, text="checkbox 1").grid(row=2, column=0, padx=20, pady=(0, 20), sticky="e")
 
-# Definizione del bottone
-button2 = ctk.Button(window, text="Testo", command=funzione_chiamata)
-# Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
-button2.grid(row=0, column=1, padx=20, pady=20)
+        # Definizione della checkbox
+        # Inserisco la checkbox nella griflia, facendola "attaccare" sul lato a ovest (west), il padding y ha come valore una tupla, in modo da averlo solo sotto
+        self.checkbox_2 = tk.Checkbutton(self, text="checkbox 2").grid(row=2, column=1, padx=20, pady=(0, 20), sticky="w")
 
-# Definizione del bottone
-button3 = ctk.Button(window, text="Testone", command=funzione_chiamata)
-# Inserisco il bottone nella griglia con padding 20 in entrambi gli assi
-# Dando l'attributo 'sticky="ew"' (che significa east e west) faccio in modo che prenda tutto lo spazio orizzontale disponibile
-# Con columnspan=2 definisco che questa riga si "espande" su 2 colonne, posso fare lo stesso con le righe tramite 'rawspan'
-button3.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+    # Funzione chiamata dal pulsante
+    def funzione_chiamata(self):
+        print("Chiamata")
 
-# Definizione della checkbox
-checkbox_1 = ctk.Checkbutton(window, text="checkbox 1")
-# Inserisco la checkbox nella griflia, facendola "attaccare" sul lato a est, il padding y ha come valore una tupla, in modo da averlo solo sotto
-checkbox_1.grid(row=2, column=0, padx=20, pady=(0, 20), sticky="e")
-# Definizione della checkbox
-checkbox_2 = ctk.Checkbutton(window, text="checkbox 2")
-# Inserisco la checkbox nella griflia, facendola "attaccare" sul lato a ovest (west), il padding y ha come valore una tupla, in modo da averlo solo sotto
-checkbox_2.grid(row=2, column=1, padx=20, pady=(0, 20), sticky="w")
-
+root = App(500, 200)
 # Messa in loop la finestra
-window.mainloop()
+root.mainloop()
